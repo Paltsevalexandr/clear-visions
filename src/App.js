89939356b82eff6bfd7e95465fdc80dc703c4790
Paltsevaleksand <paltsevaleksandr21@gmail.com>
2024-12-01 19:46:42 -0500

    added isTelegramWebView

--- src/App.js
+++ src/App.js
@@ -1,63 +1,84 @@
-import './App.scss';
+import "./App.scss";
 import { useEffect, useState } from "react";
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import { Route, Routes } from 'react-router-dom';
-import MobileMenu from './components/MobileMenu/MobileMenu';
-import Homepage from './pages/Homepage/HomePage';
-import Gallery from './pages/Gallery/Gallery';
-import NotFound from './pages/NotFound/NotFound';
-import ContactUs from './pages/ContactUs/ContactUs';
-import InteriorPainting from './pages/InteriorPainting/InteriorPainting';
-import ExteriorPainting from './pages/ExteriorPainting/ExteriorPainting';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Route, Routes } from "react-router-dom";
+import MobileMenu from "./components/MobileMenu/MobileMenu";
+import Homepage from "./pages/Homepage/HomePage";
+import Gallery from "./pages/Gallery/Gallery";
+import NotFound from "./pages/NotFound/NotFound";
+import ContactUs from "./pages/ContactUs/ContactUs";
+import InteriorPainting from "./pages/InteriorPainting/InteriorPainting";
+import ExteriorPainting from "./pages/ExteriorPainting/ExteriorPainting";
 
 function App() {
-	const [showMenu, setShowMenu] = useState(false);
-	const [showOverlay, setShowOverlay] = useState(false);
-	const [scrollTop, setScrollTop] = useState(0);
+  const [showMenu, setShowMenu] = useState(false);
+  const [showOverlay, setShowOverlay] = useState(false);
+  const [scrollTop, setScrollTop] = useState(0);
 
-	useEffect(() => {
-		window.addEventListener('scroll', handleScroll);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
 
-		// Cleanup event listener on component unmount
-		return () => {
-			window.removeEventListener('scroll', handleScroll);
-		};
-	},[]);
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-	function handleScroll(event) {
-		const scrollTopValue = document.documentElement.scrollTop;
-	    setScrollTop(scrollTopValue);
-	}
+  function handleScroll(event) {
+    const scrollTopValue = document.documentElement.scrollTop;
+    setScrollTop(scrollTopValue);
+  }
 
-	return (
-		<div>
-			<Header setShowMenu={() => {
-				setShowMenu(!showMenu);
-				setShowOverlay(!showMenu);
-			}} 
-			 showMenu={showMenu}/>
-			 <MobileMenu showMenu={showMenu} />
-			<main className='main'>
-				<Routes>
-					<Route path="/" element={<Homepage scrollTop={scrollTop} />} />
-					<Route path="/gallery" element={<Gallery scrollTop={scrollTop} />} />
-					<Route path="/interior-painting" element={<InteriorPainting scrollTop={scrollTop} />} />
-					<Route path="/exterior-painting" element={<ExteriorPainting scrollTop={scrollTop} />} />
-					<Route path="/contact-us" element={<ContactUs scrollTop={scrollTop} />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</main>
-			<Footer />
+  function isTelegramWebView() {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    return /Telegram/i.test(userAgent);
+  }
 
-			<div className={`overlay ${showOverlay ? "active" : ""}`} 
-				onClick={() => {
-					setShowMenu(false);
-					setShowOverlay(false);
-				}}
-			/>
-		</div>
-	);
+  if (isTelegramWebView()) {
+    alert("Открыто во встроенном браузере Telegram");
+  }
+
+  return (
+    <div>
+      <Header
+        setShowMenu={() => {
+          setShowMenu(!showMenu);
+          setShowOverlay(!showMenu);
+        }}
+        showMenu={showMenu}
+      />
+      <MobileMenu showMenu={showMenu} />
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<Homepage scrollTop={scrollTop} />} />
+          <Route path="/gallery" element={<Gallery scrollTop={scrollTop} />} />
+          <Route
+            path="/interior-painting"
+            element={<InteriorPainting scrollTop={scrollTop} />}
+          />
+          <Route
+            path="/exterior-painting"
+            element={<ExteriorPainting scrollTop={scrollTop} />}
+          />
+          <Route
+            path="/contact-us"
+            element={<ContactUs scrollTop={scrollTop} />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+
+      <div
+        className={`overlay ${showOverlay ? "active" : ""}`}
+        onClick={() => {
+          setShowMenu(false);
+          setShowOverlay(false);
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
